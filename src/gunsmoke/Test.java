34d8cb4349f1cd52df857dbf78c9a648b6536d3f
iLoveJia <dq_511@sina.com>
2020-01-02 17:53:10 +0800

    dq

---
+++ src/gunsmoke/Test.java
@@ -0,0 +1,10 @@
+package gunsmoke;
+
+/**
+ * Created by yz on 2020/1/2.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello github");
+    }
+}
